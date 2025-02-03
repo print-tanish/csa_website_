c428f5f5369c56ddd5cfe14a54b82a83de940571
Bhavya Shah <gmbhacademic@gmail.com>
2025-02-04 00:17:24 +0530

    design : Remove footer logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,6 @@ export default function Footer(): JSX.Element {
   return (
     <footer className="mx-auto mt-8 flex w-full max-w-3xl items-center justify-between px-4 py-4 text-xs md:text-sm lg:px-0 lg:text-base">
         <div className="flex items-center space-x-2">
-          <CosmicLogo />
           <span className="text-zinc-700 dark:text-zinc-300">
             Made with ❤️ by the Tech team 2k24
           </span>
