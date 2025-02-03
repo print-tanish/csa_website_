3c5a6c2f500eec718b61e0cdefeb79fb0c3c3bdc
Bhavya Shah <gmbhacademic@gmail.com>
2025-02-04 00:15:02 +0530

    test : Footer logo dimensions

--- components/logos/CosmicLogo.tsx
+++ components/logos/CosmicLogo.tsx
@@ -5,7 +5,6 @@ export default function CosmicLogo(): JSX.Element {
  xmlns='http://www.w3.org/2000/svg'
  width='26'
  height='26'
- viewBox='0 0 100 100'
  preserveAspectRatio='xMidYMid meet'
 >
  <g
