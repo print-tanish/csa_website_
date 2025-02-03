58d68143df99ef8255774bd5ce6dcacf5c21f93a
Bhavya Shah <gmbhacademic@gmail.com>
2025-02-04 00:13:08 +0530

    design : fix dimensions

--- components/logos/CosmicLogo.tsx
+++ components/logos/CosmicLogo.tsx
@@ -5,7 +5,7 @@ export default function CosmicLogo(): JSX.Element {
  xmlns='http://www.w3.org/2000/svg'
  width='26'
  height='26'
- viewBox='0 0 500 500'
+ viewBox='0 0 100 100'
  preserveAspectRatio='xMidYMid meet'
 >
  <g
