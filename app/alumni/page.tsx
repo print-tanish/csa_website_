c42f3c0e6314e988039d3cb4cf136a1912b00a4c
Bhavya Shah <gmbhacademic@gmail.com>
2025-03-20 19:32:11 +0530

    Fix Alumni Map

--- app/alumni/page.tsx
+++ app/alumni/page.tsx
@@ -1,7 +1,10 @@
 'use client';
-
+import dynamic from "next/dynamic";
 import AlumniMap from '../../components/AlumniMap';
-
+const LazyMap = dynamic(() => import("@/components/AlumniMap"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
 export default function AlumniMapPage() {
   return <AlumniMap />;
 }
