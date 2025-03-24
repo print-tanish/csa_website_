365bffcb461b72d7bb4b2cdffea4b9595b7fd91a
gmbcode <gmbhacademic@gmail.com>
2025-03-24 22:46:31 +0530

    Some more changes

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -4,7 +4,7 @@ import { getGlobalData } from '../../lib/cosmic';
 import Generator from 'next/font/local';
 import Banner from '../../components/Banner';
 import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+//import Footer from '../../components/Footer';
 
 const sans = Generator({
   src: '../../fonts/Generator-Variable.ttf',
@@ -24,14 +24,12 @@ export default async function Layout({
 }: {
   children: React.ReactNode;
 }) {
-  const siteData = await getGlobalData();
+  //const siteData = await getGlobalData();
 
   return (
     <html lang="en" className={`${sans.variable} font-sans`}>
       <body className="bg-white dark:bg-zinc-950">
-        <Header name={siteData} />
         {children}
-        <Footer />
       </body>
     </html>
   );
