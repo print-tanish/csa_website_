84094c8c4984a781fe716d588d1d9d91cca3e222
tanish sthalekar <tanish.sthalekar@gmail.com>
2025-04-10 03:16:09 +0530

    imported alumni card in pages

--- app/alumni/page.tsx
+++ app/alumni/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 import dynamic from "next/dynamic";
-//import AlumniMap from '../../components/AlumniMap';
-const AlumniMap = dynamic(() => import("../../components/AlumniMap"), {
+
+const AlumniCard = dynamic(() => import("../../components/AlumniCard"), {
   ssr: false,
   loading: () => <p>Loading...</p>,
 });
-export default function AlumniMapPage() {
-  return <AlumniMap />;
+
+export default function AlumniCardPage() {
+  return <AlumniCard />;
 }
