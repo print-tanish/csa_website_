0fdcee01bbafac4d9d585f102630ed0640f0034a
tanish sthalekar <tanish.sthalekar@gmail.com>
2025-04-12 21:45:14 +0530

    added por and alumni card designs

--- app/alumni/page.tsx
+++ app/alumni/page.tsx
@@ -9,3 +9,12 @@ const AlumniCard = dynamic(() => import("../../components/AlumniCard"), {
 export default function AlumniCardPage() {
   return <AlumniCard />;
 }
+
+
+//import AlumniMap from '../../components/AlumniMap';
+// const AlumniMap = dynamic(() => import("../../components/AlumniMap"), {
+//   ssr: false,
+//   loading: () => <p>Loading...</p>,
+// });
+// export default function AlumniMapPage() {
+//   return <AlumniMap />;
