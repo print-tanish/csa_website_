5761beb7107ee019e6f4d3edd5e6352c386215a9
tanish sthalekar <tanish.sthalekar@gmail.com>
2025-04-14 00:23:07 +0530

    changing structure of pages for footer at the bottom

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import CosmicLogo from './logos/CosmicLogo';
 
 export default function Footer(): JSX.Element {
   return (
-    <footer className="mx-auto mt-8 flex w-full max-w-3xl items-center justify-between px-4 py-4 text-xs md:text-sm lg:px-0 lg:text-base">
+    <footer className="mt-auto mt-8 flex w-full max-w-3xl items-center justify-between px-4 py-4 text-xs md:text-sm lg:px-0 lg:text-base">
         <div className="flex items-center space-x-2">
           <span className="text-zinc-700 dark:text-zinc-300">
             Made with ❤️ by the Tech team 2k24
@@ -15,3 +15,4 @@ export default function Footer(): JSX.Element {
     </footer>
   );
 }
+
